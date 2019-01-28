2f222c3dd32fffed353797a935b50f15a4f059e6
Nguyễn Đức Anh <ducanhvna@outlook.com>
2019-01-28 16:32:15 +0700

    Update url static

--- RoboTradeMangermentSystem/settings.py
+++ RoboTradeMangermentSystem/settings.py
@@ -58,12 +58,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 MIDDLEWARE_CLASSES = [
 # 'django.middleware.security.SecurityMiddleware',
-'whitenoise.middleware.WhiteNoiseMiddleware',
+#'whitenoise.middleware.WhiteNoiseMiddleware',
 #...
 ]
 
